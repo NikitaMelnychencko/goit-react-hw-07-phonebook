40ffd3208b3c3832cdaf58a8b1983114c334f32e
NikitaMelnychencko <hunter6059@gmail.com>
2022-01-23 21:00:41 +0200

    +

--- src/redux/contacts/phonebook-reducer.js
+++ src/redux/contacts/phonebook-reducer.js
@@ -3,56 +3,6 @@ import { createReducer, createSlice } from '@reduxjs/toolkit';
 import { addFilter } from './phonebook-actions';
 import operation from './phonebobook-operation';
 
-// const itemReducer = createReducer([], {
-//   [operation.fetchContacts.fulfilled]: (state, { payload }) => [...payload],
-//   [operation.addContacts.fulfilled]: (state, action) => [
-//     ...state,
-//     action.payload,
-//   ],
-//   [operation.updateContacts.fulfilled]: (state, { payload }) => {
-//     const updatedContacts = [];
-//     state.forEach(el => {
-//       el.id !== payload.id
-//         ? updatedContacts.push(el)
-//         : updatedContacts.push(payload);
-//     });
-//     return updatedContacts;
-//   },
-//   [operation.dellContacts.fulfilled]: (state, { payload }) =>
-//     state.filter(contact => contact.id !== payload),
-// });
-
-// const filterReducer = createReducer('', {
-//   [addFilter]: (state, action) => action.payload,
-// });
-// const error = createReducer(null, {});
-
-// const loading = createReducer(false, {
-//   //====fetchContactsAction====//
-//   [operation.fetchContacts.pending]: () => true,
-//   [operation.fetchContacts.fulfilled]: () => false,
-//   [operation.fetchContacts.rejected]: () => false,
-//   //====addContacts====//
-//   [operation.addContacts.pending]: () => true,
-//   [operation.addContacts.fulfilled]: () => false,
-//   [operation.addContacts.rejected]: () => false,
-//   //=====updateContacts====//
-//   [operation.updateContacts.pending]: () => true,
-//   [operation.updateContacts.fulfilled]: () => false,
-//   [operation.updateContacts.rejected]: () => false,
-//   //=====dellContacts====//
-//   [operation.dellContacts.pending]: () => true,
-//   [operation.dellContacts.fulfilled]: () => false,
-//   [operation.dellContacts.rejected]: () => false,
-// });
-
-// export default combineReducers({
-//   items: itemReducer,
-//   filter: filterReducer,
-//   loading: loading,
-//   error,
-// });
-
 const contacts = createSlice({
   name: 'contacts',
   initialState: {
